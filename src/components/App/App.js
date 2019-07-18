9748b5f54ceb08774dd5280d129dc3a3155f5bc0
neilenatarajan <neilenatarajan@gmail.com>
2019-07-18 10:45:14 -0400

    removed link definition in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import About from '../About/About';
