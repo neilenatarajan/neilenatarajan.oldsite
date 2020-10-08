c2204321d5a05217313bf616284ef2cca76adafd
neilenatarajan <neilenatarajan@gmail.com>
2020-10-08 18:39:51 +0000

    Updates

---
+++ precache-manifest.78332769c04b355fd68751d024211e41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5f77e683ac2841936491b558936845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3566414c80683976049",
+    "url": "/static/css/main.d82b8f28.chunk.css"
+  },
+  {
+    "revision": "e74c456381a32a4548b6",
+    "url": "/static/js/2.2bf4c9a4.chunk.js"
+  },
+  {
+    "revision": "f3566414c80683976049",
+    "url": "/static/js/main.485a156d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
